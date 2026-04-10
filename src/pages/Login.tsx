535b9588b5cf9e2cd4aa0161ebb688d6d2b8b827
enjoy06 <152522310+enjoy06@users.noreply.github.com>
2026-04-11 05:10:05 +0700

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -52,7 +52,7 @@ const Login = () => {
       <Card className="w-full max-w-md">
         <CardHeader className="space-y-1">
           <CardTitle className="text-2xl font-bold text-center">
-            WPX
+            REALTIME
           </CardTitle>
           <CardDescription className="text-center">
             Welcome Back
